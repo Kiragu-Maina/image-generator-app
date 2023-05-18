4ddbe8772f7730cdb1d14d2bfb320806cad4d3fc
JumaKenn <jumaliko711@gmail.com>
2023-05-18 22:03:13 +0300

    added styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React, { useState } from 'react';
+import './App.css'
 
 function App() {
   const [imageURL, setImageURL] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   const generateImage = () => {
+    setIsLoading(true); // Set loading state to true
+
     // Replace 'YOUR_API_ENDPOINT' with the actual endpoint that generates the image
     fetch('https://django-server-production-602e.up.railway.app/apis/generate-image')
       .then(response => response.json())
       .then(data => {
         // Assuming the API response returns the image URL
         setImageURL(data.imageURL);
+        setIsLoading(false); // Set loading state to false after receiving the response
       })
-      .catch(error => console.log(error));
+      .catch(error => {
+        console.log(error);
+        setIsLoading(false); // Set loading state to false in case of an error
+      });
   };
 
   return (
-    <div>
+    <div className="container">
       <h1>Image Generator App</h1>
       <button onClick={generateImage}>Generate Image</button>
-      {imageURL && <img src={imageURL} alt="Generated Image" />}
+      {isLoading ? (
+        <div className="progress-bar">Loading...</div>
+      ) : (
+        imageURL && <img src={imageURL} alt="Generated Image" />
+      )}
     </div>
   );
 }
